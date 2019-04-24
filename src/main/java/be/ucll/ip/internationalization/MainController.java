ebfbf91f0d622c3f44f2a85e27fba2f609825f7c
Elke Steegmans <Elke.Steegmans@ucll.be>
2019-04-24 21:06:11 +0200

    Initial commit

---
+++ src/main/java/be/ucll/ip/internationalization/MainController.java
@@ -0,0 +1,14 @@
+package be.ucll.ip.internationalization;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/{locale:en|fr|vi}/login")
+    public String login(Model model) {
+        return "login";
+    }
+}
